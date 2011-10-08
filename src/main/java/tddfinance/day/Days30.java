14c370f186dc2c0b4acf0f0a7c659a20d23121e2
Oliver J. Saionji <oliver6600gt@yahoo.co.jp>
2011-10-09 00:53:00 +0900

    Add javadoc comment to Days30

--- src/main/java/tddfinance/day/Days30.java
+++ src/main/java/tddfinance/day/Days30.java
@@ -3,6 +3,14 @@ package tddfinance.day;
 import org.joda.time.LocalDate;
 
 public class Days30 {
+	/**
+	 * 
+	 * @param start Start date of period to calculate the number of days 
+	 * @param end End date of period to calculate the number of days
+	 * @return The number of days between start and end.
+	 * The formula is as follows, 360 x (Y2 - Y1) + 30 x (M2 - M1) + D2 - D1, 
+	 * where start = Y1/M1/D1 and end = Y2/M2/D2. Also, if D1 = 31, it will be 30 (so for D2). 
+	 */
 	static public int numberOfDays(LocalDate start, LocalDate end){
 		
 		int startDayOfMonth = start.getDayOfMonth();
