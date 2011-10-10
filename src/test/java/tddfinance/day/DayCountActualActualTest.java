22c936006300368d4b0513584783890625525839
Oliver J. Saionji <oliver6600gt@yahoo.co.jp>
2011-10-10 22:52:47 +0900

    Add a leap year test case to DayCountActualActualTest

--- src/test/java/tddfinance/day/DayCountActualActualTest.java
+++ src/test/java/tddfinance/day/DayCountActualActualTest.java
@@ -23,5 +23,8 @@ public class DayCountActualActualTest {
 
 		//Semi-annual, adjusted settlement date
 		assertEquals( 31.0 / (181*2), DayCount.fraction(DayCount.DC_ACTUALACTUAL, new LocalDate(2000, 1, 1), new LocalDate(2000, 2, 1), new LocalDate(2000, 6, 30), 2 ), 1.0e-6 );  
+
+		//leap year
+		assertEquals( 364.0 / 366, DayCount.fraction(DayCount.DC_ACTUALACTUAL, new LocalDate(2003, 2, 28), new LocalDate(2004, 2, 27), new LocalDate(2004, 2, 29)), 1.0e-6 );  
 	}
 }
