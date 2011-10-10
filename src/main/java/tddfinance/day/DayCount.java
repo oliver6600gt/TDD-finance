1e62df5fe51cf95a91d60544829ecc098df8ecf3
Oliver J. Saionji <oliver6600gt@yahoo.co.jp>
2011-10-10 22:15:28 +0900

    Add new method overloads to the DayCount class, for easier use

---
+++ src/main/java/tddfinance/day/DayCount.java
@@ -0,0 +1,52 @@
+package tddfinance.day;
+
+import org.joda.time.LocalDate;
+import org.joda.time.Months;
+import org.joda.time.Years;
+
+public class DayCount {
+	public  static final DayCountConventionInterface DC_30360        = new DayCount30360();
+//	public  static final DayCountConventionInterface DC_ACTUAL360    = new DayCountActual360();
+//	public  static final DayCountConventionInterface DC_ACTUALACTUAL = new DayCountActualActual();
+
+	/**
+	 * prohibit instantiation of this class, as it's a utility class 
+	 */
+	private DayCount(){};
+
+	/**
+	 * Day count fraction with arbitrary payment frequency
+	 */
+	static public double fraction(DayCountConventionInterface convention, LocalDate accrualStartDate, LocalDate accrualEndDate, LocalDate nextPaymentSettleDate, int paymentFrequency){
+		return convention.fraction(accrualStartDate, accrualEndDate, nextPaymentSettleDate, paymentFrequency);
+	}
+
+	/**
+	 * Day count fraction with arbitrary payment frequency, where paymentFrequency should be a divisor of 12
+	 */
+	static public double fraction(DayCountConventionInterface convention, LocalDate accrualStartDate, LocalDate accrualEndDate, int paymentFrequency){
+		if( paymentFrequency < 1 || paymentFrequency > 12 || 12 % paymentFrequency != 0)
+			throw new IllegalArgumentException( "paymentFrequency = " + paymentFrequency + " is illegal. It should be a divisor of 12" );
+			
+		LocalDate nextPaymentSettleDate = accrualStartDate.plus(Months.months(12/paymentFrequency));
+		return convention.fraction(accrualStartDate, accrualEndDate, nextPaymentSettleDate, 1);
+	}
+
+	/**
+	 * Day count fraction with annual payment frequency
+	 */
+	static public double fraction(DayCountConventionInterface convention, LocalDate accrualStartDate, LocalDate accrualEndDate, LocalDate nextPaymentSettleDate){
+		return convention.fraction(accrualStartDate, accrualEndDate, nextPaymentSettleDate, 1);
+	}
+
+
+	/**
+	 * Day count fraction with annual payment frequency
+	 */
+	static public double fraction(DayCountConventionInterface convention, LocalDate accrualStartDate, LocalDate accrualEndDate){
+		LocalDate nextPaymentSettleDate = accrualStartDate.plus(Years.ONE);
+		return convention.fraction(accrualStartDate, accrualEndDate, nextPaymentSettleDate, 1);
+	}
+
+
+}
--- src/test/java/tddfinance/day/DayCount30360Test.java
+++ src/test/java/tddfinance/day/DayCount30360Test.java
@@ -10,36 +10,36 @@ public class DayCount30360Test {
 	@Test
 	public void fractorTest() throws Exception {
 		//1 month = 30 days
-		assertEquals( 1.0/12, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2000, 2, 1), new LocalDate(2000, 7, 1), 2 ), 1.0e-6 );  
+		assertEquals( 1.0/12, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2000, 2, 1), 2 ), 1.0e-6 );  
 		
 		//1 year = 360 days
-		assertEquals( 1.0, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2001, 1, 1), new LocalDate(2001, 1, 1) ), 1.0e-6 );  
+		assertEquals( 1.0, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2001, 1, 1) ), 1.0e-6 );  
 
 		//1 year - 3 months - 7 days = 263 days
-		assertEquals( 263.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 4, 10), new LocalDate(2001, 1, 3), new LocalDate(2001, 4, 10) ), 1.0e-6 );  
+		assertEquals( 263.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 4, 10), new LocalDate(2001, 1, 3) ), 1.0e-6 );  
 
 		//in the same month
-		assertEquals( 15.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2000, 1, 16), new LocalDate(2001, 1, 1) ), 1.0e-6 );  
+		assertEquals( 15.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2000, 1, 16) ), 1.0e-6 );  
 		
 		//1 month = 30 days, and different days of month
-		assertEquals( 25.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 15), new LocalDate(2000, 2, 10), new LocalDate(2001, 1, 15) ), 1.0e-6 );  
+		assertEquals( 25.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 15), new LocalDate(2000, 2, 10) ), 1.0e-6 );  
 		
 		//2 months = 60 days, and different days of month
-		assertEquals( 74.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2000, 3, 15), new LocalDate(2001, 1, 1) ), 1.0e-6 );  
+		assertEquals( 74.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 1, 1), new LocalDate(2000, 3, 15) ), 1.0e-6 );  
 
 		//31st should be treated as 30th
-		assertEquals( 1.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 8, 31), new LocalDate(2000, 9, 1), new LocalDate(2001, 8, 31) ), 1.0e-6 );  
+		assertEquals( 1.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 8, 31), new LocalDate(2000, 9, 1) ), 1.0e-6 );  
 		
 		//31st should be treated as 30th
-		assertEquals( 30.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 7, 30), new LocalDate(2000, 8, 31), new LocalDate(2001, 7, 30) ), 1.0e-6 );  
+		assertEquals( 30.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 7, 30), new LocalDate(2000, 8, 31) ), 1.0e-6 );  
 
 		//31st should be treated as 30th
-		assertEquals( 30.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 7, 31), new LocalDate(2000, 8, 31), new LocalDate(2001, 7, 31) ), 1.0e-6 );  
+		assertEquals( 30.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 7, 31), new LocalDate(2000, 8, 31) ), 1.0e-6 );  
 		
 		//Feb 28th is 28th, should not be treated as 30th
-		assertEquals( 3.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 2, 28), new LocalDate(2000, 3, 1), new LocalDate(2001, 2, 28) ), 1.0e-6 );  
+		assertEquals( 3.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 2, 28), new LocalDate(2000, 3, 1) ), 1.0e-6 );  
 		
 		//Feb 29th is 29th, should not be treated as 30th
-		assertEquals( 2.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 2, 29), new LocalDate(2000, 3, 1), new LocalDate(2001, 2, 28) ), 1.0e-6 ); 
+		assertEquals( 2.0/360, DayCount.fraction(DayCount.DC_30360, new LocalDate(2000, 2, 29), new LocalDate(2000, 3, 1) ), 1.0e-6 ); 
 	}
 }
