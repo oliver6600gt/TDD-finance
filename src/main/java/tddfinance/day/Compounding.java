5181c36102e92b1df67a5d9d524712c7d77f996d
Oliver J. Saionji <oliver6600gt@yahoo.co.jp>
2011-11-22 22:12:09 +0900

    frequency() and period() methods for Compounding

--- src/main/java/tddfinance/day/Compounding.java
+++ src/main/java/tddfinance/day/Compounding.java
@@ -1,22 +1,52 @@
 package tddfinance.day;
 
+import org.joda.time.Days;
+import org.joda.time.Months;
+import org.joda.time.ReadablePeriod;
+import org.joda.time.Weeks;
+import org.joda.time.Years;
+
+/**
+ * An enumeration class for clearer compounding representation, 
+ * instead of using 1) int as compounding frequency, and 2) ReadablePeriod as unit compounding period length, 
+ * 
+ * <p>
+ * Benefits:<br>
+ * A) No need to create the same method with to diff argument sets - with int (frequency) and ReadablePeriod (unit period) <br>
+ * B) You can only use the set of valid values<br>
+ * <br>
+ */
 public enum Compounding {
-	ANNUAL             ("Annual Compounding"),
-	SEMI_ANNUAL        ("Semi-Annual Compounding"),
-	EVERY_FOUR_MONTH   ("Every-Four-Month Compounding"),
-	QUARTERLY          ("Quarterly Compounding"),
-	BI_MONTHLY         ("Bi-Monthly Compounding"),
-	MONTHLY            ("Monthly Compounding"),
-	WEEKLY             ("Weekly Compounding"),
-	DAILY              ("Daily Compounding");
+	ANNUAL             (1,   Years.ONE,    "Annual Compounding"),
+	SEMI_ANNUAL        (2,   Months.SIX,   "Semi-Annual Compounding"),
+	EVERY_FOUR_MONTH   (3,   Months.FOUR,  "Every-Four-Month Compounding"),
+	QUARTERLY          (4,   Months.THREE, "Quarterly Compounding"),
+	BI_MONTHLY         (6,   Months.TWO,   "Bi-Monthly Compounding"),
+	MONTHLY            (12,  Months.ONE,   "Monthly Compounding"),
+	WEEKLY             (52,  Weeks.ONE,    "Weekly Compounding"),
+	DAILY              (365, Days.ONE,     "Daily Compounding");
 	
-	private final String description;
+	private final String         description;
+	private final int            compoundingFrequency;
+	private final ReadablePeriod unitPeriodLength;
 	
-	private Compounding( String description ){
-		this.description = description;
+	private Compounding( int compoundingFrequency, ReadablePeriod unitPeriodLength, String description ){
+		this.compoundingFrequency = compoundingFrequency;
+		this.unitPeriodLength    = unitPeriodLength;
+		this.description         = description;
 	}
-	
+
 	public String toString(){
 		return description;
 	}
+
+	public int frequency(){
+		return compoundingFrequency;
+	}
+	
+	public ReadablePeriod period(){
+		return unitPeriodLength;
+	}
+
+
 }
--- src/test/java/tddfinance/day/CompoundingTest.java
+++ src/test/java/tddfinance/day/CompoundingTest.java
@@ -2,9 +2,37 @@ package tddfinance.day;
 
 import static org.junit.Assert.*;
 
+import org.joda.time.Days;
+import org.joda.time.Months;
+import org.joda.time.Weeks;
+import org.joda.time.Years;
 import org.junit.Test;
 
 public class CompoundingTest {
+
+	@Test
+	public void testFrequency() throws Exception{
+		assertEquals( 1,   Compounding.ANNUAL.frequency() );
+		assertEquals( 2,   Compounding.SEMI_ANNUAL.frequency() );
+		assertEquals( 3,   Compounding.EVERY_FOUR_MONTH.frequency() );
+		assertEquals( 4,   Compounding.QUARTERLY.frequency() );
+		assertEquals( 6,   Compounding.BI_MONTHLY.frequency() );
+		assertEquals( 12,  Compounding.MONTHLY.frequency() );
+		assertEquals( 52,  Compounding.WEEKLY.frequency() );
+		assertEquals( 365, Compounding.DAILY.frequency() );
+	}
+
+	@Test
+	public void testPeriod() throws Exception{
+		assertEquals( Years.ONE,    Compounding.ANNUAL.period() );
+		assertEquals( Months.SIX,   Compounding.SEMI_ANNUAL.period() );
+		assertEquals( Months.FOUR,  Compounding.EVERY_FOUR_MONTH.period() );
+		assertEquals( Months.THREE, Compounding.QUARTERLY.period() );
+		assertEquals( Months.TWO,   Compounding.BI_MONTHLY.period() );
+		assertEquals( Months.ONE,   Compounding.MONTHLY.period() );
+		assertEquals( Weeks.ONE,    Compounding.WEEKLY.period() );
+		assertEquals( Days.ONE,     Compounding.DAILY.period() );
+	}
 
 	@Test
 	public void testToString() throws Exception{
