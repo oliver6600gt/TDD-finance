70fd260eac8cfd8a0a2d3be188f5a27ba176532c
Oliver J. Saionji <oliver6600gt@yahoo.co.jp>
2011-12-25 21:19:19 +0900

    Disable YieldCalculator temporarily

--- src/main/java/tddfinance/pricer/YieldCalculator.java
+++ src/main/java/tddfinance/pricer/YieldCalculator.java
@@ -1,78 +1,78 @@
 package tddfinance.pricer;
 
-import java.util.List;
-import java.util.ListIterator;
+//import java.util.List;
+//import java.util.ListIterator;
+//
+//import org.joda.time.ReadablePeriod;
+//
+//import tddfinance.contract.Cashflow;
+//import tddfinance.contract.CashflowListInterface;
+//import tddfinance.curve.Curve;
 
-import org.joda.time.ReadablePeriod;
-
-import tddfinance.contract.Cashflow;
-import tddfinance.contract.CashflowListInterface;
-import tddfinance.curve.Curve;
-import tddfinance.numeral.AnnualizedPeriod;
 
 public class YieldCalculator {
 
-	/**
-	 * The future value of the quantity, when you invest it in the yield for the period with the annual compounding frequency  
-	 */
-	public static double futureValue(double quantity, double yield, ReadablePeriod period) {
-		AnnualizedPeriod p = new AnnualizedPeriod(period);
-		return quantity * Math.pow( 1.0 + yield, p.getValue() );
-	}
-
-	/**
-	 * The future value of the quantity, when you invest it in the yield for the period with the compoundingFrequency  
-	 */
-	public static double futureValue(double quantity, double yield, ReadablePeriod period, int compoundingFrequency) {
-		AnnualizedPeriod p = new AnnualizedPeriod(period);
-		return quantity * Math.pow( 1.0 + yield / compoundingFrequency, compoundingFrequency * p.getValue() );
-	}
-
-	/**
-	 * Return the annual rate of return of contract, assuming zeroCouponRateCurve remains unchanged over time
-	 */
-	public static double annualRateOfReturn ( CashflowListInterface contract, Curve zeroCouponRateCurve, ReadablePeriod periodToMaturity, ReadablePeriod paymentPeriod ) throws Exception {
-		double futureValue = 0.0;
-		double annualizedPeriodToMaturity = 0.0;
-		
-		List<Cashflow>         cashflowList = contract.cashflowList();
-		ListIterator<Cashflow> lastIterator = cashflowList.listIterator( cashflowList.size() );
-		
-		//horizontal shift - assuming the zero curve remains unchanged
-		Curve shiftZeroCurve  = zeroCouponRateCurve.horizontalShift( periodToMaturity );
-		
-		AnnualizedPeriod p = new AnnualizedPeriod(paymentPeriod);
-		double eachAnnualizedPeroid = p.getValue(); 
-		
-		//Loop in reverse - needs to calculate the annualized period "To Maturity", so it is a bit difficult in the normal order
-		for (ListIterator<Cashflow> iterator = lastIterator; iterator.hasPrevious();) {
-			Cashflow cashflow = iterator.previous();
-
-			if( cashflow.settlementDate().equals(contract.maturityDate()) ){
-				futureValue += cashflow.quantity();
-			}
-			else{
-				double yield = shiftZeroCurve.getValue( contract.maturityDate() ); 
-				futureValue += cashflow.quantity() * Math.pow( 1 + yield, annualizedPeriodToMaturity );				
-			}			
-			
-			//maybe annualized period should depend on the day count convention of the product? 
-			annualizedPeriodToMaturity += eachAnnualizedPeroid;
-
-			//horizontal shift - assuming the zero curve remains unchanged
-			shiftZeroCurve = shiftZeroCurve.horizontalShiftNegative( paymentPeriod );
-		}
-		
-		double price = new CurvePricer(zeroCouponRateCurve).price(contract);
-		return Math.pow( futureValue / price, 1.0 / annualizedPeriodToMaturity ) - 1;
-	}
-
-	/**
-	 *  Calculate the zero coupon rate of periodToMaturity from price
-	 */
-	public static double zeroCouponRate(double price, ReadablePeriod periodToMaturity, int compoundingFrequency) {
-		AnnualizedPeriod p = new AnnualizedPeriod(periodToMaturity);
-		return compoundingFrequency * ( Math.pow( 100/price, 1.0/(compoundingFrequency*p.getValue() ) ) - 1 );
-	}
+//	/**
+//	 * The future value of the quantity, when you invest it in the yield for the period with the annual compounding frequency  
+//	 */
+//	public static double futureValue(double quantity, double yield, ReadablePeriod period) {
+//		AnnualizedPeriod p = new AnnualizedPeriod(period);
+//		return quantity * Math.pow( 1.0 + yield, p.getValue() );
+//	}
+//
+//	/**
+//	 * The future value of the quantity, when you invest it in the yield for the period with the compoundingFrequency  
+//	 */
+//	public static double futureValue(double quantity, double yield, ReadablePeriod period, int compoundingFrequency) {
+//		AnnualizedPeriod p = new AnnualizedPeriod(period);
+//		return quantity * Math.pow( 1.0 + yield / compoundingFrequency, compoundingFrequency * p.getValue() );
+//	}
+//
+//	/**
+//	 * Return the annual rate of return of contract, assuming zeroCouponRateCurve remains unchanged over time
+//	 */
+//	public static double annualRateOfReturn ( CashflowListInterface contract, Curve zeroCouponRateCurve, ReadablePeriod periodToMaturity, ReadablePeriod paymentPeriod ) throws Exception {
+//		double futureValue = 0.0;
+//		double annualizedPeriodToMaturity = 0.0;
+//		
+//		List<Cashflow>         cashflowList = contract.cashflowList();
+//		ListIterator<Cashflow> lastIterator = cashflowList.listIterator( cashflowList.size() );
+//		
+//		//horizontal shift - assuming the zero curve remains unchanged
+//		Curve shiftZeroCurve  = zeroCouponRateCurve.horizontalShift( periodToMaturity );
+//		
+//		AnnualizedPeriod p = new AnnualizedPeriod(paymentPeriod);
+//		double eachAnnualizedPeroid = p.getValue(); 
+//		
+//		//Loop in reverse - needs to calculate the annualized period "To Maturity", so it is a bit difficult in the normal order
+//		for (ListIterator<Cashflow> iterator = lastIterator; iterator.hasPrevious();) {
+//			Cashflow cashflow = iterator.previous();
+//
+//			if( cashflow.settlementDate().equals(contract.maturityDate()) ){
+//				futureValue += cashflow.quantity();
+//			}
+//			else{
+//				double yield = shiftZeroCurve.getValue( contract.maturityDate() ); 
+//				futureValue += cashflow.quantity() * Math.pow( 1 + yield, annualizedPeriodToMaturity );				
+//			}			
+//			
+//			//maybe annualized period should depend on the day count convention of the product? 
+//			annualizedPeriodToMaturity += eachAnnualizedPeroid;
+//
+//			//horizontal shift - assuming the zero curve remains unchanged
+//			shiftZeroCurve = shiftZeroCurve.horizontalShiftNegative( paymentPeriod );
+//		}
+//		
+//		double price = new CurvePricer(zeroCouponRateCurve).price(contract);
+//		return Math.pow( futureValue / price, 1.0 / annualizedPeriodToMaturity ) - 1;
+//	}
+//
+//	/**
+//	 *  Calculate the zero coupon rate of periodToMaturity from price
+//	 */
+//	public static double zeroCouponRate(double price, ReadablePeriod periodToMaturity, int compoundingFrequency) {
+//		AnnualizedPeriod p = new AnnualizedPeriod(periodToMaturity);
+//		return compoundingFrequency * ( Math.pow( 100/price, 1.0/(compoundingFrequency*p.getValue() ) ) - 1 );
+//	}
 
 }
--- src/test/java/tddfinance/pricer/YieldCalculatorTest.java
+++ src/test/java/tddfinance/pricer/YieldCalculatorTest.java
@@ -1,70 +1,70 @@
 package tddfinance.pricer;
 
-import static org.junit.Assert.*;
-
-import java.util.Map;
-import java.util.TreeMap;
-
-import org.joda.time.LocalDate;
-import org.joda.time.Years;
-import org.joda.time.Months;
-import org.junit.Test;
-
-import tddfinance.contract.Bond;
-import tddfinance.contract.Currency;
-import tddfinance.curve.Curve;
-import tddfinance.curve.DiscreteCurve;
-import tddfinance.curve.FlatCurve;
+//import static org.junit.Assert.*;
+//
+//import java.util.Map;
+//import java.util.TreeMap;
+//
+//import org.joda.time.LocalDate;
+//import org.joda.time.Years;
+//import org.joda.time.Months;
+//import org.junit.Test;
+//
+//import tddfinance.contract.Bond;
+//import tddfinance.contract.Currency;
+//import tddfinance.curve.Curve;
+//import tddfinance.curve.DiscreteCurve;
+//import tddfinance.curve.FlatCurve;
 
 public class YieldCalculatorTest {
 
-	@Test
-	public void futureValueTest() throws Exception {
-		assertEquals( 105.0,    YieldCalculator.futureValue( 100, 0.05, Years.years(1) ), 1.0e-6 );	
-		assertEquals( 110.25,   YieldCalculator.futureValue( 100, 0.05, Years.years(2) ), 1.0e-6 );	
-		assertEquals( 105.0,    YieldCalculator.futureValue( 100, 0.05, Months.months(12) ), 1.0e-6 );	
-		assertEquals( 105.0625, YieldCalculator.futureValue( 100, 0.05, Months.months(12), 2 ), 1.0e-6 );	
-	}
-	
-	@Test
-	public void annualRateOfReturnTest() throws Exception {
-		LocalDate baseDate = new LocalDate(2001, 4, 1);
-		Map<LocalDate, Double> curveValues = new TreeMap<LocalDate, Double>();
-		curveValues.put(baseDate.plusYears(1), 0.038);
-		curveValues.put(baseDate.plusYears(2), 0.041);
-		curveValues.put(baseDate.plusYears(3), 0.0435);
-		curveValues.put(baseDate.plusYears(4), 0.0481);
-		curveValues.put(baseDate.plusYears(5), 0.0502);
-				
-		//zero-coupon -> reinvestment rate assuming you will get all the return from "re-"investment at maturity
-		Curve zeroCouponRates = new DiscreteCurve( baseDate, curveValues ); 
-		
-		Bond  bond = new Bond(Currency.USD, 100, 0.04, baseDate, Years.years(5) );
-		
-		assertEquals(0.0493207, YieldCalculator.annualRateOfReturn(bond, zeroCouponRates, Years.years(5), Years.years(1)), 1.0e-6);	
-	}
-
-	@Test
-	public void annualRateOfReturnYTMTest() throws Exception {
-		LocalDate baseDate = new LocalDate(2001, 4, 1);
-				
-		//zero-coupon -> reinvestment rate assuming you will get all the return from "re-"investment at maturity
-		Curve zeroCouponRates = new FlatCurve( baseDate, 0.02 ); 
-		
-		Bond  bond = new Bond(Currency.USD, 100, 0.04, baseDate, Years.years(5) );
-		
-		//If you re-invest intermidiate cashflows @YTM, YTM = annual rate of return 
-		assertEquals(0.02, YieldCalculator.annualRateOfReturn(bond, zeroCouponRates, Years.years(5), Years.years(1)), 1.0e-6);	
-	}
-	
-	@Test
-	public void zeroCouponRateTest() throws Exception {
-		assertEquals(0.014075, YieldCalculator.zeroCouponRate(98.612,  Years.years(1), 1), 1.0e-6);
-		assertEquals(0.030747, YieldCalculator.zeroCouponRate(94.123,  Years.years(2), 1), 1.0e-6);
-		assertEquals(0.035932, YieldCalculator.zeroCouponRate(89.951,  Years.years(3), 1), 1.0e-6);
-		
-		assertEquals(0.050000, YieldCalculator.zeroCouponRate(95.2381, Years.years(1), 1), 1.0e-6);
-		assertEquals(0.050000, YieldCalculator.zeroCouponRate(95.1814, Years.years(1), 2), 1.0e-6);
-		assertEquals(0.050000, YieldCalculator.zeroCouponRate(97.5610, Months.months(6), 2), 1.0e-6);
-	}
+//	@Test
+//	public void futureValueTest() throws Exception {
+//		assertEquals( 105.0,    YieldCalculator.futureValue( 100, 0.05, Years.years(1) ), 1.0e-6 );	
+//		assertEquals( 110.25,   YieldCalculator.futureValue( 100, 0.05, Years.years(2) ), 1.0e-6 );	
+//		assertEquals( 105.0,    YieldCalculator.futureValue( 100, 0.05, Months.months(12) ), 1.0e-6 );	
+//		assertEquals( 105.0625, YieldCalculator.futureValue( 100, 0.05, Months.months(12), 2 ), 1.0e-6 );	
+//	}
+//	
+//	@Test
+//	public void annualRateOfReturnTest() throws Exception {
+//		LocalDate baseDate = new LocalDate(2001, 4, 1);
+//		Map<LocalDate, Double> curveValues = new TreeMap<LocalDate, Double>();
+//		curveValues.put(baseDate.plusYears(1), 0.038);
+//		curveValues.put(baseDate.plusYears(2), 0.041);
+//		curveValues.put(baseDate.plusYears(3), 0.0435);
+//		curveValues.put(baseDate.plusYears(4), 0.0481);
+//		curveValues.put(baseDate.plusYears(5), 0.0502);
+//				
+//		//zero-coupon -> reinvestment rate assuming you will get all the return from "re-"investment at maturity
+//		Curve zeroCouponRates = new DiscreteCurve( baseDate, curveValues ); 
+//		
+//		Bond  bond = new Bond(Currency.USD, 100, 0.04, baseDate, Years.years(5) );
+//		
+//		assertEquals(0.0493207, YieldCalculator.annualRateOfReturn(bond, zeroCouponRates, Years.years(5), Years.years(1)), 1.0e-6);	
+//	}
+//
+//	@Test
+//	public void annualRateOfReturnYTMTest() throws Exception {
+//		LocalDate baseDate = new LocalDate(2001, 4, 1);
+//				
+//		//zero-coupon -> reinvestment rate assuming you will get all the return from "re-"investment at maturity
+//		Curve zeroCouponRates = new FlatCurve( baseDate, 0.02 ); 
+//		
+//		Bond  bond = new Bond(Currency.USD, 100, 0.04, baseDate, Years.years(5) );
+//		
+//		//If you re-invest intermidiate cashflows @YTM, YTM = annual rate of return 
+//		assertEquals(0.02, YieldCalculator.annualRateOfReturn(bond, zeroCouponRates, Years.years(5), Years.years(1)), 1.0e-6);	
+//	}
+//	
+//	@Test
+//	public void zeroCouponRateTest() throws Exception {
+//		assertEquals(0.014075, YieldCalculator.zeroCouponRate(98.612,  Years.years(1), 1), 1.0e-6);
+//		assertEquals(0.030747, YieldCalculator.zeroCouponRate(94.123,  Years.years(2), 1), 1.0e-6);
+//		assertEquals(0.035932, YieldCalculator.zeroCouponRate(89.951,  Years.years(3), 1), 1.0e-6);
+//		
+//		assertEquals(0.050000, YieldCalculator.zeroCouponRate(95.2381, Years.years(1), 1), 1.0e-6);
+//		assertEquals(0.050000, YieldCalculator.zeroCouponRate(95.1814, Years.years(1), 2), 1.0e-6);
+//		assertEquals(0.050000, YieldCalculator.zeroCouponRate(97.5610, Months.months(6), 2), 1.0e-6);
+//	}
 }
