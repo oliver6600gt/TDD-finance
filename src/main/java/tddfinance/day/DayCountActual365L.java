0aa08169012f65aaacb9f03700a69e39893ef775
Oliver J. Saionji <oliver6600gt@yahoo.co.jp>
2012-01-08 08:39:44 +0900

    TDDF-1 Implemented the DayCountActual365L.knownAs() method

--- src/main/java/tddfinance/day/DayCountActual365L.java
+++ src/main/java/tddfinance/day/DayCountActual365L.java
@@ -1,9 +1,18 @@
 package tddfinance.day;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.joda.time.LocalDate;
 
 public class DayCountActual365L implements DayCountConvention {
-
+	
+	private static final List<String> knownAs =  Arrays.asList( "Actual/365L", "ISMA-Year" );
+	
+	public List<String> knownAs() {
+		return new ArrayList<String>( DayCountActual365L.knownAs );
+	}
 	public double fraction(
 		LocalDate accrualStartDate,
 		LocalDate accrualEndDate, 
--- src/test/java/tddfinance/day/DayCountActual365LTest.java
+++ src/test/java/tddfinance/day/DayCountActual365LTest.java
@@ -1,5 +1,9 @@
 package tddfinance.day;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.joda.time.LocalDate;
 import org.junit.Test;
 
@@ -9,5 +13,16 @@ public class DayCountActual365LTest {
 	public void test() throws Exception{
 		DayCount.fraction(DayCount.DC_ACTUAL365L, new LocalDate(2000, 1, 1), new LocalDate(2000, 2, 1), Compounding.ANNUAL );  
 	}
+	@Test
+	public void testKnownAs() throws Exception {
+		assertEquals( Arrays.asList( "Actual/365L", "ISMA-Year" ), new DayCountActual365L().knownAs() );
+	}
 
+	@Test
+	public void testKnownAsImmutable() throws Exception {
+		DayCountActual365L dcAct365L = new DayCountActual365L();
+		dcAct365L.knownAs().add("Invalid Alias");
+
+		assertEquals( Arrays.asList( "Actual/365L", "ISMA-Year" ), dcAct365L.knownAs() );		
+	}
 }
